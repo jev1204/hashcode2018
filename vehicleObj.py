f7f5b50dc9fbe797e1ffec03d81f26ad12360963
Enno Voigt <enno.voigt@ma-design.de>
2018-03-01 21:55:44 +0100

    more

--- vehicleObj.py
+++ vehicleObj.py
@@ -7,11 +7,14 @@ class VehicleObj:
     vehicle_id = 0
     start_step = 0
     finish_step = 0
+    finished_rides = []
 
     def __init__(self, vehicle_id):
         self.vehicle_id = vehicle_id
 
     def setCurrentRide(self, start_step, ride: rideObj):
+        if (self.current_ride is not None):
+            self.finished_rides.append(self.current_ride)
         self.current_ride = ride
         self.start_step = start_step
         self.finish_step = self.start_step + self.current_ride.needed_steps
