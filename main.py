553efd4fcf1bc68c32b448a23373388bb06f01bc
Jule Martensen <jule.martensen@mail.de>
2018-03-01 21:00:47 +0100

    main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import sys
+import rideObj
 
 firstLine = []
 
@@ -16,9 +17,13 @@ def readMatrix(fileName):
     rides = firstLine[3]
     bonus = firstLine[4]
     steps = firstLine[5]
-    
 
+    rideObjects = []
 
+    for line in input:
+        rideObject = rideObj.RideObj([line[0],line[1]], [line[2], line[3]], line[4], line[5])
+        rideObjects.append(rideObject)
+        print(rideObject.calcSteps())
 
 readMatrix("dataSet/a_example.in")
 
