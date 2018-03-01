9b1d9834009b2eca880e08a72b81e475c7febe82
Enno Voigt <enno.voigt@ma-design.de>
2018-03-01 20:34:52 +0100

    added rideObj

---
+++ rideObj.py
@@ -0,0 +1,20 @@
+class RideObj:
+    startIntersect = [0,0]
+    endIntersect = [0, 0]
+    startStep = 0
+    finishStep = 0
+    neededSteps = 0
+
+    def __init__(self, startIntersect, endIntersect, startStep, finishStep):
+        self.startIntersect = startIntersect
+        self.endIntersect = endIntersect
+        self.startIntersect = startIntersect
+        self.finishStep = finishStep
+
+        self.neededSteps = self.calcSteps()
+
+    def calcSteps(self):
+        rowSteps = self.endIntersect[0] - self.startIntersect[0]
+        colSteps = self.endIntersect[1] - self.startIntersect[1]
+
+        return rowSteps * colSteps
